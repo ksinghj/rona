a200e411d4bc29689ebe3e6365e038fa84f13ea7
Kartar Singh Jabanda <kartarjabanda@hotmail.co.uk>
2020-04-10 15:31:08 +0100

    complete just need styles

--- src/components/CountryData.jsx
+++ src/components/CountryData.jsx
@@ -4,19 +4,20 @@ import { getCountryData } from "../actions";
 
 class CountryData extends React.Component {
   render() {
-    if (this.props) {
-      return (
-        <div>
-          <h2>Country Figures:</h2>
-          <div className="global-figs-flex">
-            <div>Confirmed: {this.props.countryData.confirmed}</div>
-            <div>Recovered: {this.props.countryData.recovered}</div>
-            <div>Deaths: {this.props.countryData.deaths}</div>
-          </div>
-        </div>
-      );
+    const { confirmed, recovered, deaths } = this.props.countryData;
+    if (!this.props.country) {
+      return <div>Select a country</div>;
     }
-    return <div>Select a country</div>;
+    return (
+      <div>
+        <h2>Country Figures:</h2>
+        <div className="global-figs-flex">
+          <div>Confirmed: {confirmed}</div>
+          <div>Recovered: {recovered}</div>
+          <div>Deaths: {deaths}</div>
+        </div>
+      </div>
+    );
   }
 }
 
