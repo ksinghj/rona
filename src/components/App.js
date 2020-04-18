9d241220e033d39218d849bd09e440c15be800b2
kartar singh <44725786+KJabandaa10@users.noreply.github.com>
2020-04-18 18:50:39 +0100

    start redesign
    designed on Figma

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,23 @@ import React from "react";
 import Global from "./Global";
 import SelectCountry from "./SelectCountry";
 import "../styles/styles.css";
+import Logo from "../img/logo.png";
+
 class App extends React.Component {
   render() {
     return (
       <div className="container fonted">
-        <h1 className="covid-header text-blue">
-          COVID-19 Real time statistics
-        </h1>
+        <div className="nav">
+          <h1 className="covid-header">COVID-19 Case Tracker</h1>
+          <img src={Logo} alt="biohazaard symbol" />
+        </div>
+        <div className="info text-align-left">
+          Coronavirus disease 2019 (COVID-19) is an infectious disease caused by
+          severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2). The
+          disease was first identified in December 2019 in Wuhan, the capital of
+          China's Hubei province, and has since spread globally, resulting in
+          the ongoing 2019–20 coronavirus pandemic.
+        </div>
         <Global />
         <SelectCountry />
       </div>
