18e1eb62c4005ff54dccf31cd6bd7aab4474a627
kartar singh <44725786+KJabandaa10@users.noreply.github.com>
2020-04-18 19:42:39 +0100

    up to lg - need react media

--- src/components/App.js
+++ src/components/App.js
@@ -12,19 +12,21 @@ class App extends React.Component {
           <h1 className="covid-header">COVID-19 Case Tracker</h1>
           <img src={Logo} alt="biohazaard symbol" />
         </div>
-        <div className="info text-align-left">
+        <div className="text-type__info text-align-left">
           Coronavirus disease 2019 (COVID-19) is an infectious disease caused by
           severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2). The
           disease was first identified in December 2019 in Wuhan, the capital of
           China's Hubei province, and has since spread globally, resulting in
           the ongoing 2019–20 coronavirus pandemic.
         </div>
-        <Global />
-        <div className="info text-align-left">
+        <div className="text-type__info text-align-left hide-lg-up">
           Above is the number of cases worldwide, for national cases select a
           country from the dropdown below.
         </div>
-        <SelectCountry />
+        <div className="lg-cases-flex">
+          <Global />
+          <SelectCountry />
+        </div>
       </div>
     );
   }
